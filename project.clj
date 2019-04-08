f4e16b4b975ddef437afbe2552ffacb0090fdb8b
Tyler Wilding <tyler.wilding@workiva.com>
2019-04-08 13:00:13 -0400

    Update build scripts and ancillary files

--- project.clj
+++ project.clj
@@ -2,8 +2,11 @@
   :description "A handful of Clojure utilities for easing project integration with the OpenTracing API"
   :url "https://github.com/Workiva/ichnaie"
   :license {:name "Apache License, Version 2.0"}
+
   :plugins [[lein-shell "0.5.0"]
+            [lein-cljfmt "0.6.4"]
             [lein-codox "0.10.3"]]
+
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.workiva/utiliva "0.1.1"]
                  [com.workiva/recide "1.0.0"]
@@ -21,9 +24,14 @@
 
   :global-vars {*warn-on-reflection* true}
 
-  :aliases {"docs" ["do" "clean-docs," "codox"]
-            "clean-docs" ["shell" "rm" "-rf" "./documentation"]}
+  :aliases {"docs" ["do" "clean-docs," "with-profile" "docs" "codox," "java-docs"]
+            "clean-docs" ["shell" "rm" "-rf" "./documentation"]
+            "java-docs" ["shell" "javadoc" "-d" "./documentation/java" "-notimestamp"
+                         "./java-src/ichnaie/TracingContext.java"]}
 
-  :codox {:output-path "documentation"}
+  :codox {:metadata {:doc/format :markdown}
+          :themes [:rdash]
+          :output-path "documentation/clojure"}
 
-  :profiles {:dev [{:dependencies [[criterium "0.4.3"]]}]})
+  :profiles {:dev [{:dependencies [[criterium "0.4.3"]]}]
+             :docs {:dependencies [[codox-theme-rdash "0.1.2"]]}})
